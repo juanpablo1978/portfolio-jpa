eff297fbefc5b016ee63370cafaf06cc9d1eef27
juanpablo1978 <jpandrujovich@gmail.com>
2024-05-08 19:28:31 -0300

    inicio de proyecto portfolio-jpa

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="min-h-screen bg-blue-950 text-white flex justify-center items-center">
+        hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
